7f7691fefea1528b38d3a73db0f0e03a7a0ab924
Nicolas Marien <mariennicolas@icloud.com>
2025-02-11 14:11:03 +0100

    chore: add types and basic detail

--- src/api/list.ts
+++ src/api/list.ts
@@ -1,8 +1,40 @@
 import { useDefaultHeaders } from "./headers";
 import fetch from "node-fetch";
 
+type ApiResponse = {
+  count: number;
+  nextPageCursor: string;
+  results: Article[];
+};
+
+export type Article = {
+  id: string;
+  url: string;
+  source_url: string;
+  title: string;
+  author: string;
+  source: string;
+  category: string;
+  location: string;
+  tags: Record<string, unknown>; // Assuming tags can be an object with unknown structure
+  site_name: string;
+  word_count: number;
+  created_at: string; // ISO 8601 date string
+  updated_at: string; // ISO 8601 date string
+  notes: string;
+  published_date: string; // Format: YYYY-MM-DD
+  summary: string;
+  image_url: string;
+  parent_id: string | null; // Can be a string or null
+  reading_progress: number; // Assuming a float value between 0 and 1
+  first_opened_at: string | null; // Can be a string or null
+  last_opened_at: string | null; // Can be a string or null
+  saved_at: string; // ISO 8601 date string
+  last_moved_at: string; // ISO 8601 date string
+};
+
 export async function list(location = "shortlist") {
-  const readerAPI = `https://readwise.io/api/v3/list?location=${location}`;
+  const readerAPI = `https://readwise.io/api/v3/list?location=${location}&category=article`;
 
   const headers = useDefaultHeaders();
 
@@ -11,6 +43,5 @@ export async function list(location = "shortlist") {
     headers: headers,
   });
 
-  console.log(response.status);
-  return await response.json();
+  return (await response.json()) as ApiResponse;
 }
--- src/list-articles.tsx
+++ src/list-articles.tsx
@@ -1,20 +1,53 @@
 import { useEffect, useState } from "react";
-import { list } from "./api/list";
-import { List } from "@raycast/api";
+import { Article, list } from "./api/list";
+import { Icon, List } from "@raycast/api";
 
+function getProgressIcon(readingProgress: number) {
+  const asPercentage = readingProgress * 100;
+  if (asPercentage === 0) {
+    return Icon.Circle;
+  } else if (asPercentage === 100) {
+    return Icon.CircleProgress100;
+  } else if (asPercentage > 0 && asPercentage <= 50) {
+    return Icon.CircleProgress25;
+  } else if (asPercentage > 50 && asPercentage < 75) {
+    return Icon.CircleProgress50;
+  } else {
+    return Icon.CircleProgress75;
+  }
+}
 export default function ListArticleCommand() {
-  const [articles, setArticles] = useState<Record<string, string>[]>([]);
+  const [articles, setArticles] = useState<Article[]>([]);
 
   useEffect(() => {
     list().then((response) => {
-      setArticles(response.results);
+      const sortedArticles = response.results.sort(
+        (a, b) => new Date(b.saved_at).getTime() - new Date(a.saved_at).getTime()
+      );
+      setArticles(sortedArticles);
     });
   }, []);
 
   return (
-    <List>
+    <List isShowingDetail>
       {articles.map((article) => (
-        <List.Item key={article.id} title={article.title} subtitle={article.site_name} />
+        <List.Item
+          key={article.id}
+          title={article.title}
+          subtitle={article.site_name}
+          detail={
+            <List.Item.Detail
+              markdown={`
+# ${article.title}
+
+![](${article.image_url})
+            
+${article.summary}
+            `}
+            />
+          }
+          icon={getProgressIcon(article.reading_progress)}
+        />
       ))}
     </List>
   );
