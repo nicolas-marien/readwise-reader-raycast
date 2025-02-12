5dc9910dae1c4165790b91e28cb2b4b015716263
Nicolas Marien <mariennicolas@icloud.com>
2025-02-12 20:12:53 +0100

    feat: add multiple location filtering

--- src/api/list.ts
+++ src/api/list.ts
@@ -1,3 +1,4 @@
+import { Article } from "../utils/article";
 import { useDefaultHeaders } from "./headers";
 import fetch from "node-fetch";
 
@@ -7,33 +8,7 @@ type ApiResponse = {
   results: Article[];
 };
 
-export type Article = {
-  id: string;
-  url: string;
-  source_url: string;
-  title: string;
-  author: string;
-  source: string;
-  category: string;
-  location: string;
-  tags: Record<string, unknown>; // Assuming tags can be an object with unknown structure
-  site_name: string;
-  word_count: number;
-  created_at: string; // ISO 8601 date string
-  updated_at: string; // ISO 8601 date string
-  notes: string;
-  published_date: string; // Format: YYYY-MM-DD
-  summary: string;
-  image_url: string;
-  parent_id: string | null; // Can be a string or null
-  reading_progress: number; // Assuming a float value between 0 and 1
-  first_opened_at: string | null; // Can be a string or null
-  last_opened_at: string | null; // Can be a string or null
-  saved_at: string; // ISO 8601 date string
-  last_moved_at: string; // ISO 8601 date string
-};
-
-export async function list(location = "shortlist") {
+export async function list(location: Article["location"] = "new") {
   const readerAPI = `https://readwise.io/api/v3/list?location=${location}&category=article`;
 
   const headers = useDefaultHeaders();
--- src/list-articles.tsx
+++ src/list-articles.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
-import { Article, list } from "./api/list";
-import { Icon, List } from "@raycast/api";
+import { list } from "./api/list";
+import { Action, ActionPanel, Icon, List } from "@raycast/api";
+import { type Article } from "./utils/article";
 
 function getProgressIcon(readingProgress: number) {
   const asPercentage = readingProgress * 100;
@@ -16,20 +17,39 @@ function getProgressIcon(readingProgress: number) {
     return Icon.CircleProgress75;
   }
 }
+
+const ArticleLocationDropdown: React.FunctionComponent<{
+  onLocationChange: (location: Article["location"]) => unknown;
+}> = (props) => {
+  return (
+    <List.Dropdown
+      tooltip="Location if the article to fetch"
+      onChange={(value) => props.onLocationChange(value as Article["location"])}
+    >
+      <List.Dropdown.Item title="New" value="new" />
+      <List.Dropdown.Item title="Shortlist" value="shortlist" />
+      <List.Dropdown.Item title="Feed" value="feed" />
+      <List.Dropdown.Item title="Later" value="later" />
+      <List.Dropdown.Item title="Archive" value="archive" />
+    </List.Dropdown>
+  );
+};
 export default function ListArticleCommand() {
+  const [articleLocation, setArticleLocation] = useState<Article["location"]>("new");
   const [articles, setArticles] = useState<Article[]>([]);
 
   useEffect(() => {
-    list().then((response) => {
+    list(articleLocation).then((response) => {
+      console.log(response.results);
       const sortedArticles = response.results.sort(
         (a, b) => new Date(b.saved_at).getTime() - new Date(a.saved_at).getTime()
       );
       setArticles(sortedArticles);
     });
-  }, []);
+  }, [articleLocation]);
 
   return (
-    <List isShowingDetail>
+    <List isShowingDetail searchBarAccessory={<ArticleLocationDropdown onLocationChange={setArticleLocation} />}>
       {articles.map((article) => (
         <List.Item
           key={article.id}
@@ -47,6 +67,12 @@ ${article.summary}
             />
           }
           icon={getProgressIcon(article.reading_progress)}
+          actions={
+            <ActionPanel title={article.title}>
+              <Action.OpenInBrowser url={article.url} title="Open article in Readwise" />
+              <Action.OpenInBrowser url={article.source_url} title="Open article in source website" />
+            </ActionPanel>
+          }
         />
       ))}
     </List>
