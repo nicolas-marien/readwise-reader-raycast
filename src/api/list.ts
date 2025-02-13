8778fc6d17343080b2f4d0129aebec0395efd038
Nicolas Marien <mariennicolas@icloud.com>
2025-02-13 14:54:43 +0100

    feat: handle rate limit

--- src/api/list.ts
+++ src/api/list.ts
@@ -1,3 +1,4 @@
+import { showToast, Toast } from "@raycast/api";
 import { Document } from "../utils/document";
 import { useDefaultHeaders } from "./headers";
 import fetch from "node-fetch";
@@ -8,7 +9,7 @@ type ApiResponse = {
   results: Document[];
 };
 
-export async function list(location: Document["location"], cursor?: string) {
+export async function list(location: Document["location"], cursor?: string): Promise<ApiResponse> {
   const readerAPI = `https://readwise.io/api/v3/list?${new URLSearchParams({
     location,
     ...(cursor ? { pageCursor: cursor } : {}),
@@ -20,6 +21,21 @@ export async function list(location: Document["location"], cursor?: string) {
     method: "GET",
     headers: headers,
   });
+
+  if (response.status === 429) {
+    const retryAfter = Number(response.headers.get("Retry-After"));
+    if (!isNaN(retryAfter)) {
+      await showToast({
+        message: "The API call has been rate limited, it will restart shortly",
+        title: "Rate limit",
+        style: Toast.Style.Animated,
+      });
+      await new Promise((resolve) => setTimeout(resolve, retryAfter * 1000)); // wait for x seconds
+      return list(location, cursor); // send new request
+    } else {
+      throw new Error("Invalid Retry-After header value");
+    }
+  }
 
   return (await response.json()) as ApiResponse;
 }
